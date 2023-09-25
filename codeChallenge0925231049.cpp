e6c126a7864fb74eb7b988969cd21089a829df0a
Mag024 <141672938+Mag024@users.noreply.github.com>
2023-09-25 12:22:23 -0700

    Add files via upload

---
+++ codeChallenge0925231049.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+
+    class Node{
+public:
+    int Value;
+    Node* Next;
+    };
+
+    void printList (Node* n){
+    while (){
+        cout << n->Value;
+    }
+    }
+
+    int main {
+        Node* head = new Node()
+        Node* second = new Node()
+        Node* third = new Node()
+        Node* fourth = new Node()
+
+        head->Value = Hyena;
+        head->Next = second;
+
+        second->Value = Bear;
+        second->Next = third;
+
+        head->Value = Lion;
+        head->Next = third;
+
+        head->Value = Tiger;
+        second->Next = NULL;
+
+        }
+    return 0;
+}
